cdc8c29d79bd21ce6bd325584f524dbcc3fc6870
juan <juan@juan-laptop.(none)>
2009-12-22 19:52:25 -0300

    actualize portfolio.php

--- portfolio.php
+++ portfolio.php
@@ -17,9 +17,9 @@
 
                 <div class="portfolio" >            
                     <div class="characteristics_portfolio">
-                        <h4>AZER</h4>
-                        <h5>XHTML / CSS / AJAX</h5>
-                        <p>La actividad industrial del tercer trimestre de 2009 con relaci&oacute;n al segundo trimestre de 2009 registr&oacute; un incremento del 0,9% en t&eacute;rminos desestacionalizados, seg&uacute;n indic&oacute; hoy el Indec. Con relaci&oacute;n a septiembre de 2008, la producci&oacute;n manufacturera mostr&oacute; un incremento del 0,1% en la medici&oacute;n con estacionalidad y una disminuci&oacute;n del 0,5% en t&eacute;rminos desestacionalizados.</p><br /><br />
+                        <h4>AZER CONSULTORES</h4>
+                        <h5>Dise&ntilde;o, xhtml, css, php, js, jquery, flash, cms</h5>
+                        <p>Constituye una empresa de consultoria en recursos humanos y búsqueda de personal. Nos encomendaron la tarea de rediseño de su web y la creación de un CMS para la administración de sus contenidos. La web cuenta con animaciones, efectos de transición, formulario de contacto y hasta la posibilidad del envio de CV para los postulantes de manera online.</p><br /><br />
                 
                         <a href="http://www.azerconsultores.com" target="_blank">www.azerconsultores.com</a>
                     </div>
@@ -30,8 +30,8 @@
                 <div class="portfolio">
                     <div class="characteristics_portfolio">
                         <h4>GETSOUTH</h4>
-                        <h5>XHTML / CSS / AJAX</h5>
-                        <p>La actividad industrial del tercer trimestre de 2009 con relaci&oacute;n al segundo trimestre de 2009 registr&oacute; un incremento del 0,9% en t&eacute;rminos desestacionalizados, seg&uacute;n indic&oacute; hoy el Indec. Con relaci&oacute;n a septiembre de 2008, la producci&oacute;n manufacturera mostr&oacute; un incremento del 0,1% en la medici&oacute;n con estacionalidad y una disminuci&oacute;n del 0,5% en t&eacute;rminos desestacionalizados.</p><br /><br />
+                        <h5>Dise&ntilde;o, xhtml, css, php, js, jquery, cms</h5>
+                        <p>Getsouth es una guia turistica de Sud America la cual incluye diversas areas de interes e informacion para turistas. Posee anunciantes por país, región y ciudad, además de su debida inclusión dentro de la categoría que corresponde a cada anunciante. Cuenta con catalogo de anunciantes, administración de banners publicitarios y modulos laterales, búsqueda básica y avanzada dentro de la misma web, CMS para la administración de todo el contenido y creación de nuevas categorías de anunciantes y de destinos.</p><br /><br />
 
                         <a href="http://www.getsouth.com" target="_blank">www.getsouth.com</a>
                     </div>
@@ -41,9 +41,33 @@
 
                 <div class="portfolio">
                     <div class="characteristics_portfolio">
+                        <h4>AIKIDO ARGENTINA</h4>
+                        <h5>Dise&ntilde;o, xhtml, css, php, js, jquery</h5>
+                        <p>AikidoArgentina es una web perteneciente a un dojo donde se practica el arte marcial de defensa personal AIKIDO, dentro de su web se muestra informacion de las practicas ademas de su historia como arte marcial, eventos, galerias de imagenes y videos, ubicacion, mas varios datos de interes general. Se realizo el redise&ntilde;o completo, sumando algunas secciones extras, agregando funcionalidades y trabajando con una forma sencilla de motrar los contenidos.</p><br /><br />
+
+                        <a href="http://www.aikidoargentina.com" target="_blank">www.aikidoargentina.com</a>
+                    </div>
+                    <div class="image_portfolio"><img src="images/portfolio/latinoamericaexporta.png"  alt="Latino America Exporta" /></div>
+                </div>
+                <!--end .portfolio-->
+
+                <div class="portfolio">
+                    <div class="characteristics_portfolio">
+                        <h4>FEDERICO AZPILCUETA - ARTE</h4>
+                        <h5>Dise&ntilde;o, xhtml, css, php, js, jquery, cms</h5>
+                        <p>Esta web pertenece a un pintor reconocido el cual nos encomendo la tarea de realizarle su web para mostrar los trabajos que realize, asi como algo de informaci&oacute;n de su biograf&iacute;a y datos de contacto. Para la web se busco un dise&ntilde;o minimalista y limpio. El sitio contiene galer&iacute;as de sus obras, exhibiciones realizadas con CMS para la administraci&oacute;n de todos los contenidos en 2 idiomas (espa&ntilde;ol e ingl&eacute;s)</p><br /><br />
+
+                        <a href="http://www.federicoazpilcueta.com" target="_blank">www.federicoazpilcueta.com</a>
+                    </div>
+                    <div class="image_portfolio"><img src="images/portfolio/latinoamericaexporta.png"  alt="Latino America Exporta" /></div>
+                </div>
+                <!--end .portfolio-->
+
+                <div class="portfolio">
+                    <div class="characteristics_portfolio">
                         <h4>LYC Consultora</h4>
-                        <h5>XHTML / CSS / AJAX</h5>
-                        <p>La actividad industrial del tercer trimestre de 2009 con relaci&oacute;n al segundo trimestre de 2009 registr&oacute; un incremento del 0,9% en t&eacute;rminos desestacionalizados, seg&uacute;n indic&oacute; hoy el Indec. Con relaci&oacute;n a septiembre de 2008, la producci&oacute;n manufacturera mostr&oacute; un incremento del 0,1% en la medici&oacute;n con estacionalidad y una disminuci&oacute;n del 0,5% en t&eacute;rminos desestacionalizados.</p><br /><br />
+                        <h5>Dise&ntilde;o, xhtml, css, flash</h5>
+                        <p>LyC CONSULTORA es una firma argentina, que con poco más de 10 años de existencia, ha brindado servicios a un amplio espectro de empresas e industrias, consolidándola en un crecimiento que la convierte en una firma altamente competitiva. Se realizo un diseño basado en la imagen seria y profesional que deseaban transmitir. MyDesign tambien se encargo en este caso de la redaccion de los textos basandose en entrevistas con integrantes de la empresa.</p><br /><br />
                 
                         <a href="http://www.lycconsultora.com.ar" target="_blank">www.lycconsultora.com.ar</a>
                     </div>
@@ -54,26 +78,15 @@
                  <div class="portfolio">
                     <div class="characteristics_portfolio">
                         <h4>Fundaci&oacute;n Proambiente</h4>
-                        <h5>XHTML / CSS / AJAX</h5>
-                        <p>La actividad industrial del tercer trimestre de 2009 con relaci&oacute;n al segundo trimestre de 2009 registr&oacute; un incremento del 0,9% en t&eacute;rminos desestacionalizados, seg&uacute;n indic&oacute; hoy el Indec. Con relaci&oacute;n a septiembre de 2008, la producci&oacute;n manufacturera mostr&oacute; un incremento del 0,1% en la medici&oacute;n con estacionalidad y una disminuci&oacute;n del 0,5% en t&eacute;rminos desestacionalizados.</p><br /><br />
+                        <h5>Dise&ntilde;o, xhtml, css, js</h5>
+                        <p>Es un organismo no gubernamental sin fines de lucro, creado para instrumentar y ejecutar programas de desarrollo sustentable para el ambiente, la industria y la sociedad en general. En este caso se trabajo sobre el rediseño de una web antigua manteniendo algunas imagenes que ya poseia, editando otras y generando algunas imagenes nuevas. La web se realizo de forma estatica y sin animaciones, incluye formulario para asociarse, objetivos y actividades</p><br /><br />
                 
-                        <a href="http://www.fundaproambiente.org.ar" target="_blank">www.fundaproambiente.org.ar</a>
+                        <a href="http://www.proambiente.org.ar" target="_blank">www.proambiente.org.ar</a>
                     </div>
                     <div class="image_portfolio"><img src="images/portfolio/proambiente.png"  alt="Fundación Proambiente" /></div>
                 </div>
                 <!--end .portfolio-->
 
-                <div class="portfolio">
-                    <div class="characteristics_portfolio">
-                        <h4>Latino America Exporta</h4>
-                        <h5>XHTML / CSS / AJAX</h5>
-                        <p>La actividad industrial del tercer trimestre de 2009 con relaci&oacute;n al segundo trimestre de 2009 registr&oacute; un incremento del 0,9% en t&eacute;rminos desestacionalizados, seg&uacute;n indic&oacute; hoy el Indec. Con relaci&oacute;n a septiembre de 2008, la producci&oacute;n manufacturera mostr&oacute; un incremento del 0,1% en la medici&oacute;n con estacionalidad y una disminuci&oacute;n del 0,5% en t&eacute;rminos desestacionalizados.</p><br /><br />
-                
-                        <a href="http://www.latinoamericaexporta.com" target="_blank">www.latinoamericaexporta.com</a>
-                    </div>
-                    <div class="image_portfolio"><img src="images/portfolio/latinoamericaexporta.png"  alt="Latino America Exporta" /></div>
-                </div>
-                <!--end .portfolio-->
             </div>
             <!--end .servicios-->
             
